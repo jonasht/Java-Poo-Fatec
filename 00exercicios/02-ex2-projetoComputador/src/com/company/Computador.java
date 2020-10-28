90f2236488f056c4a6eb57e7ea42c3ba7d8b264e
jonasht <jhenriquet@outlook.com.br>
2020-10-28 00:07:08 -0300

    add

--- 00exercicios/02-ex2-projetoComputador/src/com/company/Computador.java
+++ 00exercicios/02-ex2-projetoComputador/src/com/company/Computador.java
@@ -8,12 +8,31 @@ public class Computador {
     public double preco;
 
     public void imprimir(){
+        System.out.println("===================================");
+        System.out.println("marca = " + marca);
+        System.out.println("cor = " + cor);
+        System.out.println("modelo = " + modelo);
+        System.out.println("numeroSerie = " + numeroSerie);
+        System.out.println("preço = " + preco);
+        System.out.println("===================================");
+
+
+
 
     }
     public void calcularValor(){
-
+        if (marca == "HP"){
+            preco *= 1.30;
+        }
+        if (marca == "IBM"){
+            preco *= 1.50;
+        }
     }
-    public void alterarValor(){
-
+    public int alterarValor(double v){
+        if (v > 0){
+            preco = v;
+            return 1;
+        }
+        return 0;
     }
 }
--- 00exercicios/02-ex2-projetoComputador/src/com/company/ComputadorTeste.java
+++ 00exercicios/02-ex2-projetoComputador/src/com/company/ComputadorTeste.java
@@ -4,5 +4,40 @@ public class ComputadorTeste {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("\n\n\nfeito no windows \n\n");
+
+        Computador p1 = new Computador();
+        p1.marca = "HP";
+        p1.cor = "amarelo";
+        p1.modelo = "alguom";
+        p1.numeroSerie = 6466;
+        p1.preco = 20000.50f; // crise do coronavirus
+
+        p1.imprimir();
+        p1.calcularValor();
+        p1.imprimir();
+
+
+        Computador p2 = new Computador();
+        p2.marca = "IBM";
+        p2.cor = "preto";
+        p2.modelo = "antigo thinkpad";
+        p2.numeroSerie = 648866;
+        p2.preco = 20000.50f; // crise do coronavirus
+
+        p2.imprimir();
+        p2.calcularValor();
+        p2.imprimir();
+
+        Computador p3 = new Computador();
+        p3.marca = "purism";
+        p3.cor = "preto";
+        p3.modelo = "seguro";
+        p3.numeroSerie = 648866;
+        p3.preco = 30000.70f; // crise do coronavirus
+
+        int r = p3.alterarValor(50000f);
+        if (r == 1) System.out.println("valor alterado com sucesso");
+    else System.out.println("valor não alterado");
     }
 }
