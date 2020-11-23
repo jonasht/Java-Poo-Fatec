33859911794fa33f904ea6b0cc88c408eee01b3d
jonasht <jhenriquet@outlook.com.br>
2020-11-23 15:06:25 -0300

    add para salvar arquivo

---
+++ 20-projetosPacotes/src/com/company/GeranciarEscola.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class GeranciarEscola {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("projetos pacotes");
+    }
+}
---
+++ 20-projetosPacotes/src/com/company/model/Aluno.java
@@ -0,0 +1,30 @@
+package com.company.model;
+
+public class Aluno {
+    private int ra;
+    private String nome;
+
+    public int getRa() {
+        return ra;
+    }
+
+    public void setRa(int ra) {
+        this.ra = ra;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "ra=" + ra +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+}
